b07750af1492c4afa1acbe80a49cbdbafee51999
Tom <mushroomfleet@gmail.com>
2024-03-15 17:04:11 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .simple_source_file import SIMPLE_CUSTOM_NODE
+NODE_CLASS_MAPPINGS = { "AspectSize" : SimpleCustomNode }
+NODE_DISPLAY_NAME_MAPPINGS = { "AspectSize" : "Aspect Size" }
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
