d58c478c156f0a06bbd3554a11429168c027c494
Tom <mushroomfleet@gmail.com>
2024-08-26 21:51:29 +0100

    Add files via upload
    init update

--- __init__.py
+++ __init__.py
@@ -15,6 +15,18 @@ try:
 except ImportError:
     print("Unable to import AspectSizeV2. This node will not be available.")
 
+try:
+    from .ImageSizeAdjuster import ImageSizeAdjuster
+    NODE_CLASS_MAPPINGS["ImageSizeAdjuster"] = ImageSizeAdjuster
+    NODE_DISPLAY_NAME_MAPPINGS["ImageSizeAdjuster"] = "Image Size Adjuster"
+except ImportError:
+    print("Unable to import ImageSizeAdjuster. This node will not be available.")
 
+try:
+    from .ImageSizeAdjusterV2 import ImageSizeAdjusterV2
+    NODE_CLASS_MAPPINGS["ImageSizeAdjusterV2"] = ImageSizeAdjusterV2
+    NODE_DISPLAY_NAME_MAPPINGS["ImageSizeAdjusterV2"] = "Image Size Adjuster V2"
+except ImportError:
+    print("Unable to import ImageSizeAdjusterV2. This node will not be available.")
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
